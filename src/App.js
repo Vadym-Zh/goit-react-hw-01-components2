c61ae84765e67150cf4e6d4ea696b4e36fe685a4
Vadym Zhuravlov <vadiazh@gmail.com>
2023-06-05 21:24:57 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // import Normalize from 'react-normalize';
 import './App.css';
 import { Profile } from './components/Profile/Profile';
-import { Statistics } from './components/Statistics1/Statistics';
+import { Statistics } from './components/Statistics/Statistics';
 import { FriendList } from './components/FriendList/FriendList';
 import { TransactionHistory } from './components/TtransactionHistory/TransactionHistory';
 import {
